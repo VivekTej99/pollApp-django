719a30761608a5928dbd1cb5025c6f65b6c4d2a5
vivek <vivektej2299@gmail.com>
2022-05-26 13:01:47 +0530

    Add str represents for poll models

--- demosite/polls/models.py
+++ demosite/polls/models.py
@@ -6,8 +6,18 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published')
 
+    def __str__(self) -> str:
+        return f"<Question(question_text={self.question_text},\
+                date_published={self.pub_date}\
+                )"
+
 class Choice(models.Model):
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    
+
+    def __str__(self) -> str:
+        return f"<Choice(choice_text={self.choice_text},\
+                votes={self.votes}\
+                )"
+
