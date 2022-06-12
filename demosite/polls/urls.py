3cbcd8700433c87bfe5420db07e33363eec658a2
vivek <vivektej2299@gmail.com>
2022-06-12 18:30:39 +0530

    Add views and templates for poll index.html

--- demosite/polls/urls.py
+++ demosite/polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name="poll_main")
+    path('', views.main, name="poll_main"),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- demosite/polls/views.py
+++ demosite/polls/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 
 def main(request):
+    latest_question_list = Question.objects.order_by('-pub_date')
+    context = {
+        "latest_question_list": latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
     return HttpResponse("Hey! polls have been setup.")
+
+def detail(request, question_id):
+    return HttpResponse("Yah!, you are in question detail page %s." %question_id)
+
+def results(request, question_id):
+    return HttpResponse("Yah!, you are in question results page %s." %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Yah!, you are in question vote page %s." %question_id)
